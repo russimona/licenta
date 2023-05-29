7b5dee502eb4268d5d22d46d1b8b37ebc53f3259
russimona <russimona1200@gmail.com>
2023-05-29 19:19:39 +0300

    no message

--- services/forgotPassword.ts
+++ services/forgotPassword.ts
@@ -2,6 +2,6 @@ import { IResetPasswordData } from "@/utils/interface";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 export const resetPassword = async (props: IResetPasswordData) => {
-    const auth = getAuth();
-    return sendPasswordResetEmail(auth, props.email)
+  const auth = getAuth();
+  return sendPasswordResetEmail(auth, props.email);
 };
